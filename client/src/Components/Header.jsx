e9c1b5db31da38275b7b0e17b56f0c8dfe0c29a0
Omar <omarhasani_@hotmail.com>
2025-05-29 18:28:44 -0400

    hotfix: increased Header margin bottom

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
 
     return(
         <>
-            <div className="text-center">
+            <div className="text-center mb-3">
                 <h1 className="font-semibold text-3xl tracking-tight">MyPrereqs</h1>
             </div>
         </>
